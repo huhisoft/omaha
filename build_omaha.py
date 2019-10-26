ea152ed7ea92af4e78e61b8ef53a3097ca330613
huhisoft <huhibrowser@gmail.com>
2019-10-26 10:36:35 +0700

    add test

---
+++ build_omaha.py
@@ -0,0 +1,207 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import argparse
+import os
+import os.path
+import shutil
+import subprocess as sp
+import sys
+
+
+def FetchThirdParties(args, omaha_dir):
+  os.chdir(omaha_dir)
+
+  command = ['git', 'submodule', 'update', '--init']
+  sp.check_call(command, stderr=sp.STDOUT)
+
+def Build(args, omaha_dir, build_all):
+  # move to omaha/omaha and start build.
+  os.chdir(os.path.join(omaha_dir, 'omaha'))
+
+  # set signing environment variables
+  key_pfx_path = os.environ.get('KEY_PFX_PATH', '')
+  key_cer_path = os.environ.get('KEY_CER_PATH', '')
+  authenticode_password = os.environ.get('AUTHENTICODE_PASSWORD', '')
+
+  mode = 'opt-win'
+  if build_all:
+    mode = 'all'
+  command = ['hammer-brave.bat', 'MODE=' + mode, '--all', '--sha2_authenticode_file=' + key_pfx_path,
+    '--sha2_authenticode_password=' + authenticode_password, '--sha1_authenticode_file=' + key_pfx_path,
+    '--sha1_authenticode_password=' + authenticode_password, '--patching_certificate=' + key_cer_path,
+    '--authenticode_file=' + key_pfx_path, '--authenticode_password=' + authenticode_password]
+
+  sp.check_call(command, stderr=sp.STDOUT)
+
+def Copy_Untagged_Installers(args, omaha_dir, debug):
+  last_win_dir = 'opt-win'
+  if debug:
+    last_win_dir = 'dbg-win'
+  omaha_out_dir = os.path.join(omaha_dir, 'omaha', 'scons-out', last_win_dir)
+
+  source_untagged_installer = os.path.join(omaha_out_dir, 'Test_Installers', 'UNOFFICIAL_' + args.untagged_installer_exe[0])
+  target_untagged_installer_file = args.untagged_installer_exe[0]
+  if debug:
+    target_untagged_installer_file = 'Debug' + target_untagged_installer_file
+  target_untagged_installer = os.path.join(args.root_out_dir[0], target_untagged_installer_file)
+
+  shutil.copyfile(source_untagged_installer, target_untagged_installer)
+
+  source_untagged_stub_installer = os.path.join(omaha_out_dir, 'staging', 'BraveUpdateSetup.exe')
+  target_untagged_stub_installer_file = args.stub_untagged_exe[0]
+  if debug:
+    target_untagged_stub_installer_file = 'Debug' + target_untagged_stub_installer_file
+  target_untagged_stub_installer = os.path.join(args.root_out_dir[0], target_untagged_stub_installer_file)
+
+  shutil.copyfile(source_untagged_stub_installer, target_untagged_stub_installer)
+
+def PrepareStandalone(args, omaha_dir):
+  # copy brave installer to create standalone installer.
+  installer_file = os.path.join(args.root_out_dir[0], args.brave_installer_exe[0])
+  shutil.copyfile(installer_file, os.path.join(omaha_dir, 'omaha', 'standalone', args.brave_installer_exe[0]))
+  shutil.copyfile(installer_file, os.path.join(omaha_dir, 'omaha', 'standalone', args.untagged_installer_exe[0]))
+  shutil.copyfile(installer_file, os.path.join(omaha_dir, 'omaha', 'standalone', args.silent_installer_exe[0]))
+
+  # prepare manifset file.
+  f = open(os.path.join(omaha_dir, 'manifest_template.gup'),'r')
+  filedata = f.read()
+  f.close()
+
+  newdata = filedata.replace("APP_GUID", args.guid[0])
+  newdata = newdata.replace("BRAVE_INSTALLER_EXE", args.brave_installer_exe[0])
+  newdata = newdata.replace("INSTALL_SWITCH", args.install_switch[0])
+
+  target_manifest_file = args.guid[0] + '.gup'
+  target_manifest_path = os.path.join(omaha_dir, 'omaha', 'standalone', 'manifests', target_manifest_file)
+  f = open(target_manifest_path,'w')
+  f.write(newdata)
+  f.close()
+
+  # update standalone_installers.txt.
+  installer_text = "('STANDALONE_FILE_NAME', 'STANDALONE_FILE_NAME', [('BRAVE_VERSION', '$MAIN_DIR/standalone/BRAVE_INSTALLER_EXE', 'APP_GUID')], None, None, None, False, '', '')"
+  installer_text = installer_text.replace("APP_GUID", args.guid[0])
+  installer_text = installer_text.replace("STANDALONE_FILE_NAME", os.path.splitext(args.standalone_installer_exe[0])[0])
+  installer_text = installer_text.replace("BRAVE_INSTALLER_EXE", args.brave_installer_exe[0])
+  installer_text = installer_text.replace("BRAVE_VERSION", args.brave_full_version[0])
+
+  # add untagged installer info to standalone_installers.txt.
+  untagged_installer_text = "('UNTAGGED_FILE_NAME', 'UNTAGGED_FILE_NAME', [('BRAVE_VERSION', '$MAIN_DIR/standalone/UNTAGGED_INSTALLER_EXE', 'APP_GUID')], None, None, None, False, '', '')"
+  untagged_installer_text = untagged_installer_text.replace("APP_GUID", args.guid[0])
+  untagged_installer_text = untagged_installer_text.replace("UNTAGGED_FILE_NAME", os.path.splitext(args.untagged_installer_exe[0])[0])
+  untagged_installer_text = untagged_installer_text.replace("UNTAGGED_INSTALLER_EXE", args.untagged_installer_exe[0])
+  untagged_installer_text = untagged_installer_text.replace("BRAVE_VERSION", args.brave_full_version[0])
+
+  # add silent installer info to standalone_installers.txt.
+  silent_installer_text = "('SILENT_FILE_NAME', 'SILENT_FILE_NAME', [('BRAVE_VERSION', '$MAIN_DIR/standalone/SILENT_INSTALLER_EXE', 'APP_GUID')], None, None, None, False, '', '')"
+  silent_installer_text = silent_installer_text.replace("APP_GUID", args.guid[0])
+  silent_installer_text = silent_installer_text.replace("SILENT_FILE_NAME", os.path.splitext(args.silent_installer_exe[0])[0])
+  silent_installer_text = silent_installer_text.replace("SILENT_INSTALLER_EXE", args.silent_installer_exe[0])
+  silent_installer_text = silent_installer_text.replace("BRAVE_VERSION", args.brave_full_version[0])
+
+  target_installer_text_path = os.path.join(omaha_dir, 'omaha', 'standalone', 'standalone_installers.txt')
+  f = open(target_installer_text_path,'w')
+  f.write(installer_text + '\n')
+  f.write(untagged_installer_text + '\n')
+  f.write(silent_installer_text)
+  f.close()
+
+def Tagging(args, omaha_dir, debug):
+  last_win_dir = 'opt-win'
+  if debug:
+    last_win_dir = 'dbg-win'
+  omaha_out_dir = os.path.join(omaha_dir, 'omaha', 'scons-out', last_win_dir)
+  apply_tag_exe = os.path.join(omaha_out_dir, 'obj', 'tools', 'ApplyTag', 'ApplyTag.exe')
+
+  tag_admin = os.environ.get('TAG_ADMIN', 'prefers')
+  tag = 'appguid=APP_GUID&appname=TAG_APP_NAME&needsadmin=TAG_ADMIN&lang=en&ap=TAG_AP'
+  tag = tag.replace("TAG_ADMIN", tag_admin)
+  tag = tag.replace("APP_GUID", args.guid[0])
+  tag = tag.replace("TAG_APP_NAME", args.tag_app_name[0])
+  tag = tag.replace("TAG_AP", args.tag_ap[0])
+
+  silent_tag = 'appguid=APP_GUID&appname=TAG_APP_NAME&needsadmin=TAG_ADMIN&lang=en&ap=TAG_AP&silent'
+  silent_tag = silent_tag.replace("TAG_ADMIN", 'False')
+  silent_tag = silent_tag.replace("APP_GUID", args.guid[0])
+  silent_tag = silent_tag.replace("TAG_APP_NAME", args.tag_app_name[0])
+  silent_tag = silent_tag.replace("TAG_AP", args.tag_ap[0])
+
+  source_standalone_installer = os.path.join(omaha_out_dir, 'Test_Installers', 'UNOFFICIAL_' + args.standalone_installer_exe[0])
+  target_standalone_installer_file = args.standalone_installer_exe[0]
+  if debug:
+    target_standalone_installer_file = 'Debug' + target_standalone_installer_file
+  target_standalone_installer = os.path.join(args.root_out_dir[0], target_standalone_installer_file)
+  command = [apply_tag_exe, source_standalone_installer, target_standalone_installer, tag]
+  sp.check_call(command, stderr=sp.STDOUT)
+
+  source_silent_installer = os.path.join(omaha_out_dir, 'Test_Installers', 'UNOFFICIAL_' + args.silent_installer_exe[0])
+  target_silent_installer_file = args.silent_installer_exe[0]
+  if debug:
+    target_silent_installer_file = 'Debug' + target_silent_installer_file
+  target_silent_installer = os.path.join(args.root_out_dir[0], target_silent_installer_file)
+  command = [apply_tag_exe, source_silent_installer, target_silent_installer, silent_tag]
+  sp.check_call(command, stderr=sp.STDOUT)
+
+  source_stub_installer = os.path.join(omaha_out_dir, 'staging', 'BraveUpdateSetup.exe')
+  target_stub_installer_file = args.stub_installer_exe[0]
+  if debug:
+    target_stub_installer_file = 'Debug' + target_stub_installer_file
+  target_stub_installer = os.path.join(args.root_out_dir[0], target_stub_installer_file)
+  command = [apply_tag_exe, source_stub_installer, target_stub_installer, tag]
+  sp.check_call(command, stderr=sp.STDOUT)
+
+  source_silent_stub_installer = os.path.join(omaha_out_dir, 'staging', 'BraveUpdateSetup.exe')
+  target_silent_stub_installer_file = args.stub_silent_exe[0]
+  if debug:
+    target_silent_stub_installer_file = 'Debug' + target_silent_stub_installer_file
+  target_silent_stub_installer = os.path.join(args.root_out_dir[0], target_silent_stub_installer_file)
+  command = [apply_tag_exe, source_silent_stub_installer, target_silent_stub_installer, silent_tag]
+  sp.check_call(command, stderr=sp.STDOUT)
+  return
+
+def ParseArgs():
+  parser = argparse.ArgumentParser(description='build omaha installer')
+  parser.add_argument('--root_out_dir',
+                      nargs=1)
+  parser.add_argument('--brave_installer_exe',
+                      nargs=1)
+  parser.add_argument('--stub_installer_exe',
+                      nargs=1)
+  parser.add_argument('--stub_untagged_exe',
+                      nargs=1)
+  parser.add_argument('--stub_silent_exe',
+                      nargs=1)
+  parser.add_argument('--standalone_installer_exe',
+                      nargs=1)
+  parser.add_argument('--silent_installer_exe',
+                      nargs=1)
+  parser.add_argument('--untagged_installer_exe',
+                      nargs=1)
+  parser.add_argument('--guid',
+                      nargs=1)
+  parser.add_argument('--install_switch',
+                      nargs=1)
+  parser.add_argument('--tag_ap',
+                      nargs=1)
+  parser.add_argument('--tag_app_name',
+                      nargs=1)
+  parser.add_argument('--brave_full_version',
+                      nargs=1)
+  return parser.parse_args()
+
+def Main(args):
+  args = ParseArgs()
+  omaha_dir = os.path.join(args.root_out_dir[0], '..', '..', 'brave', 'vendor', 'omaha')
+
+  FetchThirdParties(args, omaha_dir)
+  PrepareStandalone(args, omaha_dir)
+  Build(args, omaha_dir, False)
+  Tagging(args, omaha_dir, False)
+  Copy_Untagged_Installers(args, omaha_dir, False)
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(Main(sys.argv))
---
+++ omaha/base/const_object_names.h
@@ -0,0 +1,149 @@
+// Copyright 2007-2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+//
+// Kernel object names.
+
+#ifndef OMAHA_BASE_CONST_OBJECT_NAMES_H_
+#define OMAHA_BASE_CONST_OBJECT_NAMES_H_
+
+#include <tchar.h>
+#include "omaha/base/constants.h"
+
+namespace omaha {
+
+// The prefix to use for global names in the win32 API's.
+const TCHAR* const kGlobalPrefix = _T("Global\\BraveSoftware");
+
+const TCHAR* const kObjectName64Suffix = _T("-x64");
+
+const TCHAR* const kCrashPipeNamePrefix =
+    _T("\\\\.\\pipe\\") SHORT_COMPANY_NAME _T("CrashServices");
+
+// Ensures that only one instance of machine or user Omaha is trying to setup at
+// a time.
+const TCHAR* const kSetupMutex = _T("{C50974A0-5616-4DC6-AC6D-D4EFF6F5FAC3}");
+
+// TODO(omaha3): Update this comment.
+// Signals the process to exit. Currently the core and the worker listen to
+// this event.
+// TODO(omaha): Consider making all our processes listen to it. Maybe not the
+// service, since the SCM controls the life time of the service.
+const TCHAR* const kShutdownEvent =
+    _T("{4613C8D6-D26E-4F10-B494-72CFF6F0BF0B}");
+
+// This is for Omaha2 backwards compatibility.
+// The installed Omaha3 handoff process sets an event to tell an Omaha2 setup
+// worker running from the temp directory that a UI has been displayed so that
+// the Omaha2 worker will not display a second UI on error. The event's name is
+// passed in this environment variable name by the Omaha2 worker.
+const TCHAR* const kLegacyUiDisplayedEventEnvironmentVariableName =
+    _T("GOOGLE_UPDATE_UI_DISPLAYED_EVENT_NAME");
+
+// Ensures the Core only runs one instance per machine and one instance per
+// each user session.
+const TCHAR* const kCoreSingleInstance =
+    _T("{EC98B00C-9557-4627-ADCF-5832C3B09AA3}");
+
+// Ensures the Crash Handler only runs one instance per machine and one
+// instance per each user session.
+const TCHAR* const kCrashHandlerSingleInstance =
+    _T("{08DA086F-9FC4-4B2E-954C-6D7D5ACD5167}");
+
+// Ensures the /ua process only runs one instance per machine and one
+// instance per each user session.
+const TCHAR* const kUpdateAppsSingleInstance =
+    _T("{0EADE80E-E9B8-4A5D-AF64-6D2A918F597C}");
+
+// Ensures only one installer for an app is running in a session.
+// The %s is replaced with the application ID.
+const TCHAR* const kInstallAppSingleInstance =
+    _T("%s-{0684C3E2-4EFA-4D1C-AE8D-A61945B94687}");
+
+// Ensures the GoogleUpdate3 server only runs one instance per machine and one
+// instance per each user session.
+const TCHAR* const kGoogleUpdate3SingleInstance =
+    _T("{BD1D9A71-3C5B-436B-BAD8-D337E3226819}");
+
+// TODO(omaha): Delete Job Object code.
+
+// Base name of job object for Setup phase 1 processes except self updates.
+// These may not be running as Local System for machine installs like
+// self-updates do.
+const TCHAR* const kSetupPhase1NonSelfUpdateJobObject =
+    _T("{4748B871-BD66-46D8-AAA1-BB61817AFF8F}");
+
+// Base name of job object for interactive install processes except /install.
+const TCHAR* const kAppInstallJobObject =
+    _T("{0495DC7E-EA56-4677-9F0B-3B1535DF0E1A}");
+
+// Base name of job object for silent processes that are okay to kill.
+const TCHAR* const kSilentJobObject =
+    _T("{FB4AA39D-3174-43A2-838C-2DA4523D8B55}");
+
+// Base name of job object for silent processes that should not be killed.
+const TCHAR* const kSilentDoNotKillJobObject =
+    _T("{9D178053-EB3B-4183-B258-385F139CC1E8}");
+
+// The global lock to ensure that a single app is being installed for this
+// user/machine at a given time.
+const TCHAR* const kInstallManagerSerializer =
+    _T("{FE0E7F6B-B8BD-4EEE-A8F1-8CE625AEF520}");
+
+// Serializes access to metrics stores, machine and user, respectively.
+const TCHAR* const kMetricsSerializer =
+    _T("{D6025E95-A77B-4ADB-B46F-65CC31BB40E7}");
+
+// Serializes access to the registry for application state.
+const TCHAR* const kRegistryAccessMutex =
+    _T("{4E15433F-5E08-47A1-AA4F-B1D1657EE725}");
+
+// Serializes opt user id generation.
+const TCHAR* const kOptUserIdLock =
+    _T("{BC6A0F04-AE75-459F-B879-2C961515B78A}");
+
+// Prefix used for programs with external (in-process) updaters to signal to
+// Omaha that they are currently doing an update check, and that Omaha should
+// not attempt to update it at this time.  (Conversely, it's also used by Omaha
+// to signal that Omaha is about to perform an update check, and that the
+// in-process updater should back off.)   The app ID is appended to this string,
+// and the standard prefixes for Omaha events are prepended.  For example, if
+// machine Earth was performing an in-process update, it would create:
+//   Global\\OmahaUpdaterRunning{74AF07D8-FB8F-4D51-8AC7-927721D56EBB} // NOLINT
+// While user Chrome would create two objects:
+//   Global\\OmahaS-1-5-21-39260824-743453154-142223018-386460UpdaterRunning{4DC8B4CA-1BDA-483E-B5FA-D3C12E15B62D}  // NOLINT
+//   Global\\OmahaS-1-5-21-39260824-743453154-142223018-386460UpdaterRunning{8A69D345-D564-463C-AFF1-A69D9E530F96}  // NOLINT
+//
+// The Omaha client will check for these events and not add these apps to the
+// bundle when performing an update check.  These events are also checked in
+// the COM server by the worker (as part of AppStateInit::QueueUpdateCheck);
+// if they are held when the worker runs, we will immediately transition to
+// AppStateError with code GOOPDATE_E_APP_USING_IN_PROCESS_UPDATER.
+const TCHAR* const kExternalUpdaterActivityPrefix =
+    _T("UpdaterRunning");
+
+// The name of the shared memory objects containing the serialized COM
+// interface pointers exposed by the machine core.
+// TODO(omaha): Rename these constants to remove "GoogleUpdate".
+// TODO(omaha): Consider following the kGlobalPrefix convention with the 'G'
+// for the new shared Omaha 3 name  and building this from the same #define as
+// kGlobalPrefix.
+const TCHAR* const kGoogleUpdate3SharedMemoryName =
+    _T("Global\\") APP_NAME_IDENTIFIER _T("3");
+const TCHAR* const kGoogleUpdateCoreSharedMemoryName =
+    _T("Global\\") APP_NAME_IDENTIFIER _T("Core");
+
+}  // namespace omaha
+
+#endif  // OMAHA_BASE_CONST_OBJECT_NAMES_H_
